8e2cb64f99458122f2d2e71ff6b2d761309a9142
Endang.Ismaya <endang.ismaya@gmail.com>
2020-07-11 10:44:04 +0700

    20200711: add form to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string;
+  date: string;
+  amount: number;
+
+  onNameChange(value: string): void {
+    this.name = value;
+  }
+
+  onDateChange(value: string): void {
+    this.date = value;
+  }
+
+  onAmountChange(value: string): void {
+    this.amount = parseFloat(value);
+  }
 }
